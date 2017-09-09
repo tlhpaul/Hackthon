91949deb0f4db834bf63c9dc83636dd02de96ed4
tlhpaul <tsehsu@seas.upenn.edu>
2017-09-08 21:23:31 -0400

    add

--- app/src/main/java/com/example/paulhsu/hackthon/MainActivity.java
+++ app/src/main/java/com/example/paulhsu/hackthon/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.paulhsu.hackthon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLaunchButton(View v) {
+        Intent i = new Intent(this, DisplayActivity.class);
+
+
     }
 
 }

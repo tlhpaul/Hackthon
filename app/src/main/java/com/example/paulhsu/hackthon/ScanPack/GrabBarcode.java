5ec3f2edfdc4c79e023accfa2c5da990a1309038
samweinberg23 <samweinberg23@gmail.com>
2017-09-09 12:26:37 -0400

    grab barcode

--- app/src/main/java/com/example/paulhsu/hackthon/ScanPack/GrabBarcode.java
+++ app/src/main/java/com/example/paulhsu/hackthon/ScanPack/GrabBarcode.java
@@ -1,63 +1,42 @@
 package com.example.paulhsu.hackthon.ScanPack;
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.SparseArray;
 import android.view.View;
 
 import com.example.paulhsu.hackthon.R;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class GrabBarcode extends Activity {
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+    private SparseArray<Barcode> barcode = new SparseArray<>();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_display);
-        requestPermission();
     }
 
-    private void requestPermission(){
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
-
-            } else {
-
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        }
+    public void scanBarcode(View v){
+        Intent intent = new Intent(this, Scan.class);
+        startActivityForResult(intent, 0);
     }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 0){
+            if(resultCode == CommonStatusCodes.SUCCESS){
+                if(data != null){
+                    barcode = data.getParcelableExtra("barcode");
                 }
-                return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    public SparseArray<Barcode> getBarcode() {
+        return barcode;
     }
 }
 
--- app/src/main/java/com/example/paulhsu/hackthon/ScanPack/Scan.java
+++ app/src/main/java/com/example/paulhsu/hackthon/ScanPack/Scan.java
@@ -2,21 +2,28 @@ package com.example.paulhsu.hackthon.ScanPack;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import com.example.paulhsu.hackthon.R;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
+
 class Scan extends Activity {
     SurfaceView cameraPreview;
+    private Boolean permissionGranted = false;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -25,6 +32,7 @@ class Scan extends Activity {
         if(ContextCompat.checkSelfPermission(this,
                 Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED){
+            permissionGranted = true;
             getCameraSource();
         }
     }
@@ -37,19 +45,22 @@ class Scan extends Activity {
         if(!detector.isOperational()){
             return;
         }
-        final CameraSource cameraSource = new CameraSource.Builder(this, detector)
+        final CameraSource cameraSource = new CameraSource.Builder(Scan.this, detector)
                 .setAutoFocusEnabled(true)
                 .build();
         cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                try {
-                    cameraSource.start(cameraPreview.getHolder());
-                }catch (Exception e){
-
+                if(ActivityCompat.checkSelfPermission(Scan.this, Manifest.permission.CAMERA)
+                        !=PackageManager.PERMISSION_GRANTED) {
+                    return;
                 }
-            }
+                    try {
+                        cameraSource.start(cameraPreview.getHolder());
+                    } catch (IOException e) {
 
+                    }
+                }
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
@@ -57,7 +68,24 @@ class Scan extends Activity {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
 
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if(barcodes.size() > 0){
+                    Intent intent = new Intent();
+                    intent.putExtra("barcode", barcodes.valueAt(0));
+                    setResult(CommonStatusCodes.SUCCESS, intent);
+                    finish();
+                }
             }
         });
     }

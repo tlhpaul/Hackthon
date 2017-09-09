8eac0122d188e9263b606a9e26f6d432278ac9f5
samweinberg23 <samweinberg23@gmail.com>
2017-09-09 08:28:59 -0400

    update camera

--- app/src/main/java/com/example/paulhsu/hackthon/ScannerPack/Scan.java
+++ app/src/main/java/com/example/paulhsu/hackthon/ScannerPack/Scan.java
@@ -4,7 +4,10 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,13 +18,18 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import com.example.paulhsu.hackthon.R;
 
+import java.io.IOException;
+
 public class Scan extends AppCompatActivity {
+    SurfaceView cameraPreview;
+    private static final String SCANTAG = "0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_display);
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button scanButton = (Button) findViewById(R.id.button);
+        cameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             }
@@ -43,12 +51,34 @@ public class Scan extends AppCompatActivity {
         return interpretBitmap(detector);
     }
     private SparseArray<Barcode> interpretBitmap(BarcodeDetector detector){
-        Bitmap myBitmap = grabBitMap(detector);
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        return detector.detect(frame);
+//        Bitmap myBitmap = grabBitMap(detector);
+//        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+//        return detector.detect(frame);
+        return new SparseArray<Barcode>();
     }
 
-    private Bitmap grabBitMap(BarcodeDetector detector){
-        final CameraSource cameraSource = new CameraSource.Builder(this, detector).build();
+    private void grabBitMap(BarcodeDetector detector){ //will be bitmap
+        final CameraSource cameraSource = new CameraSource.Builder(this, detector)
+                .build();
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback(){
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    cameraSource.start(cameraPreview.getHolder());
+                }catch (IOException e){
+                    Log.e(SCANTAG, "Camera Permission still not available");
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
     }
 }

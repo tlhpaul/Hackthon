843c4bfb3cd0a458f758d63f2f07a5faf03faad4
samweinberg23 <samweinberg23@gmail.com>
2017-09-08 22:14:51 -0400

    visualize barchart

--- app/src/main/java/com/example/paulhsu/hackthon/NutrientPack/NutrientDisplay.java
+++ app/src/main/java/com/example/paulhsu/hackthon/NutrientPack/NutrientDisplay.java
@@ -1,7 +1,37 @@
 package com.example.paulhsu.hackthon.NutrientPack;
 
+import android.app.Activity;
 import android.widget.RelativeLayout;
 
-public class NutrientDisplay {
-    RelativeLayout displayLayout;
+import com.example.paulhsu.hackthon.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+
+import java.util.ArrayList;
+
+
+public class NutrientDisplay extends Activity {
+    private BarChart chart = (BarChart)findViewById(R.id.chart);
+    private ArrayList<BarEntry> entries = new ArrayList<>();
+
+    private void updateEntries(ArrayList<Integer> nutrients){
+        if(entries.size() != nutrients.size()) {
+            for (int i = 0; i < nutrients.size(); i++) {
+                entries.add(new BarEntry(nutrients.get(i), i));
+            }
+        }else{
+            for(int i = 0; i < nutrients.size(); i++){
+                entries.get(i).setX(entries.get(i).getX() + nutrients.get(i));
+            }
+        }
+    }
+    public void updateVisual(ArrayList<Integer> nutrients){
+        BarDataSet dataSet = new BarDataSet(entries, "Nutrient percent");
+        BarData barData = new BarData(dataSet);
+        chart.setData(barData);
+        chart.invalidate();
+    }
 }

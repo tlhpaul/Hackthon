a79e451b9e2a6aebbfc0b4ab09c47dca198ffe21
samweinberg23 <samweinberg23@gmail.com>
2017-09-08 21:12:50 -0400

    tracker

---
+++ app/src/main/java/com/example/paulhsu/hackthon/NutrientTracker.java
@@ -0,0 +1,24 @@
+package com.example.paulhsu.hackthon;
+
+import java.util.ArrayList;
+
+public class NutrientTracker {
+
+    private int days;
+    private ArrayList<Integer> freeNutrients;
+
+    public NutrientTracker(int days, ArrayList<Integer> freeNutrients){
+        this.days = days;
+        this.freeNutrients = freeNutrients;
+        for(int nutrient: freeNutrients){
+            nutrient *= days;
+        }
+    }
+    public int getDays() {
+        return days;
+    }
+
+    public ArrayList<Integer> getFreeNutrients() {
+        return freeNutrients;
+    }
+}

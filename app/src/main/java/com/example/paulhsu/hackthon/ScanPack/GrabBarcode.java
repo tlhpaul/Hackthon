a3eb17283a157685da48aca7538602c8dcb943e3
samweinberg23 <samweinberg23@gmail.com>
2017-09-09 10:25:21 -0400

    access scan

--- app/src/main/java/com/example/paulhsu/hackthon/ScanPack/GrabBarcode.java
+++ app/src/main/java/com/example/paulhsu/hackthon/ScanPack/GrabBarcode.java
@@ -1,4 +1,15 @@
 package com.example.paulhsu.hackthon.ScanPack;
 
-public class GrabBarcode {
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.paulhsu.hackthon.R;
+
+public class GrabBarcode extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_display);
+    }
 }
+

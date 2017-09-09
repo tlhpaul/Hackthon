d6cf5212fbb2e2e6e83187c4eb5caa33606217d5
tlhpaul <tsehsu@seas.upenn.edu>
2017-09-08 21:31:22 -0400

    push

--- app/src/main/java/com/example/paulhsu/hackthon/MainActivity.java
+++ app/src/main/java/com/example/paulhsu/hackthon/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.paulhsu.hackthon.NutrientTrackerMain.DisplayActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
